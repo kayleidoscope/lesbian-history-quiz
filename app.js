7e00adcc639d90cce25fc3f921fbd17943cefdab
Kay Holten <holtenkay@gmail.com>
2020-09-09 10:57:58 -0700

    initial upload

---
+++ app.js
@@ -0,0 +1,108 @@
+/**
+ * Example store structure
+ */
+const store = {
+  // 5 or more questions are required
+  questions: [
+    {
+      question: 'Which of the following was NOT a late 1800s term for so-called romantic friendship between women?',
+      answers: [
+        'smashes',
+        'crushes',
+        'spoons',
+        'squeezes'
+      ],
+      correctAnswer: 'squeezes'
+    },
+    {
+      question: '`The Well of Loneliness` is a novel about the love between an upper-class woman named Stephen Gordon and Mary Llewellyn, an ambulance driver. What year was it published?',
+      answers: [
+        '1928',
+        '1899',
+        '1953',
+        '1970'
+      ],
+      correctAnswer: '1928'
+    },
+    {
+      question: 'What percent of women in 1929 admitted to having had intense emotional relations with other women?',
+      answers: [
+        '50.4%',
+        '24.7%',
+        '12.9%',
+        '2.8%'
+      ],
+      correctAnswer: '50.4%'
+    },
+    {
+      question: 'Which word in the 1930s referred to a woman who strayed into lesbianism as second-best but stayed because she found she preferred it to heterosexuality?',
+      answers: [
+        'twist',
+        'loop',
+        'spook',
+        'canary'
+      ],
+      correctAnswer: 'spook'
+    },
+    {
+      question: 'Which of these was a reason that a female sergeant in WWII gave for allowing lesbians in the military?',
+      answers: [
+        'The military needed every man and woman the country could spare.',
+        'Fewer pregnancies occurred with lesbians around.',
+        'The government would be able to reeducate them in the military.',
+        'They deserved equal rights.'
+      ],
+      correctAnswer: 'Fewer pregnancies occurred with lesbians around.'
+    },
+    {
+      question: 'What 1950s term described a lesbian who was neither a butch nor a femme?',
+      answers: [
+        'razz',
+        'newt',
+        'Nora',
+        'kiki'
+      ],
+      correctAnswer: 'kiki'
+    }
+  ],
+  quizStarted: false,
+  questionNumber: 0,
+  score: 0
+};
+
+/**
+ * 
+ * Technical requirements:
+ * 
+ * Your app should include a render() function, that regenerates the view each time the store is updated. 
+ * See your course material and access support for more details.
+ *
+ * NO additional HTML elements should be added to the index.html file.
+ *
+ * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
+ *
+ * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
+ * 
+ */
+
+/********** TEMPLATE GENERATION FUNCTIONS **********/
+
+// These functions return HTML templates
+
+/********** RENDER FUNCTION(S) **********/
+
+// This function conditionally replaces the contents of the <main> tag based on the state of the store
+
+/********** EVENT HANDLER FUNCTIONS **********/
+
+// These functions handle events (submit, click, etc)
+
+
+
+
+
+
+
+
+//Mount Holyoke Students photo credit: Kitty Ely class of 1887 (left) and Helen Emory class of 1889, Mount Holyoke students, via vintagephoto.livejournal.com
+//Mabel Hampton and Lillian Foster, circa 1940
